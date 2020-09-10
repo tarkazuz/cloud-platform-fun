b802f21a93092debc0504aa2478571cd4692034a
C5309084 <sueleyman.bilgi@sap.com>
2020-09-10 13:43:11 +0200

    remove DB_CONNECTION_STRING env variable, since it should be part of the tech specific exercise
    Co-authored-by: marco.spiess01@sap.com <marco.spiess01@sap.com>

--- src/main.js
+++ src/main.js
@@ -3,10 +3,8 @@
 const createApp = require('./app');
 const FortuneCookieService = require('./fortune-cookie-service');
 
-const connectionString = process.env.DB_CONNECTION_STRING;
-
 const config = {
-    connectionString: connectionString || 'postgres://postgres@localhost/postgres'
+    connectionString: 'postgres://postgres@localhost/postgres'
 };
 
 createApp(new FortuneCookieService(config)).listen(process.env.PORT || 3000);
